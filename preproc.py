33d68a73f5145c28448d9489a5efba081a142aa9
hamil168 <34875630+hamil168@users.noreply.github.com>
2018-07-21 14:53:52 -0700

    Refactoring import dataset

--- preproc.py
+++ preproc.py
@@ -24,9 +24,9 @@ import time
 
 # - DATA PREPROCESSING ##########
 
-# Importing the dataset
-lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
-conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+# Importing the dataset (do this in the ipynb)
+# lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+# conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
 
 
 def id_to_line(lines):
